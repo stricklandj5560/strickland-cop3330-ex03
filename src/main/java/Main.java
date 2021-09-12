957b74a44325f587af51f4e33640bbc23c37b9d1
Jonathan Strickland <76930188+stricklandj5560@users.noreply.github.com>
2021-09-12 15:55:21 -0400

    added in author

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jonathan Strickland
+ */
 public class Main {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
